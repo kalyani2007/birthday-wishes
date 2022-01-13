22b4c0e4d8950b75e9e488440fc3f806fa7d4401
kalyani2007 <87479029+kalyani2007@users.noreply.github.com>
2022-01-13 21:34:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Button, View, Text } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+        <Text style={{marginTop:50,color: 'white',
+	textAlign: 'center', 
+	fontWeight: 'bold',
+	fontSize: 50,
+	backgroundColor: 'pink',width: 300,
+  height: 300,
+  borderBottomLeftRadius:30,
+ textShadowColor: 'black', 
+ fontFamily :'Times Now Roman',
+    /*Add the shadowcolor */
+    
+  textShadowOffset:{width: 10, height: 10},
+    textShadowRadius:8,
+    }}>HAPPY BIRTHDAY MY DEAR FRIEND:)</Text>
+  
+    );
+  }
+}
+
+
